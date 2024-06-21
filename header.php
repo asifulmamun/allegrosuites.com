9e5c9529017fc3b481e37812174e74d9af672196
asifulmamun <asifulmamun@gmail.com>
2024-06-21 20:11:37 +0600

    font awesome update

--- header.php
+++ header.php
@@ -17,7 +17,8 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <!-- <link rel="profile" href="https://gmpg.org/xfn/11"> -->
 <!-- Font-Awesome -->
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.2/css/font-awesome.min.css">
+<!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.2/css/font-awesome.min.css"> -->
+<script src="https://kit.fontawesome.com/95c622a962.js" crossorigin="anonymous"></script>
 <!-- Bootstrap CSS -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
 <?php wp_head(); ?>
